444db6d216b123c918fab4878ca5145c88dcca18
Abu Solayman Sefat <sefat01625@gmail.com>
2026-05-14 22:32:18 +0600

    explore the database connection and query execution here before starting the server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { type Application, type Request, type Response } from 'express';
-import {Pool} from "pg"
+import { Pool } from "pg"
 const app: Application = express();
 const port = 3000;
 
@@ -7,9 +7,32 @@ app.use(express.json());
 
 const pool = new Pool({
       connectionString:
-      "postgresql://neondb_owner:npg_k06shIcDLaoJ@ep-rapid-dew-aplchtdf-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-})
+            "postgresql://neondb_owner:npg_k06shIcDLaoJ@ep-rapid-dew-aplchtdf-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
+const initDb = async () => {
+      try {
+            await pool.query(`
+                  CREATE TABLE IF NOT EXISTS users(
+                  id SERIAL PRIMARY KEY,
+                  name VARCHAR(20),
+                  email VARCHAR(20) UNIQUE NOT NULL,
+                  password VARCHAR(20) NOT NULL,
+                  is_active BOOLEAN DEFAULT true,
+                  age INT,
+                  
+                  created_at_timestamp TIMESTAMPTZ DEFAULT NOW(),
+                  updated_at_timestamp TIMESTAMPTZ DEFAULT NOW()
+                  )
+                  `)
+            console.log("database initialized successfully");
+      } catch (error) {
+            console.log(error)
+      }
+}
+
+
+initDb();
 app.get('/', (req: Request, res: Response) => {
       //   res.send('Express + typescript server');
       res.status(200).json({
@@ -20,13 +43,28 @@ app.get('/', (req: Request, res: Response) => {
 
 app.post("/", async (req: Request, res: Response) => {
       // console.log(req.body);
-      const { name, email, password } = req.body;
+      const { name, email, password, age } = req.body;
+
+      try {
+            const result = await pool.query(`
+            INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4) RETURNING *
+            `, [name, email, password, age]);
+            // console.log(result);
+
       res.status(201).json({
-            message: "created successfully",
-            data: { name, email },
+
+            message: "user created successfully",
+            data: result.rows[0],
       })
+      }catch (error: any) {
+            res.status(500).json({
+                  message: error.message || "something went wrong",
+                  error: error
+            })
+      }
 
 })
+
 
 app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
